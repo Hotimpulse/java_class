82f58525d65cc9808bd91de408d8acf5815e3c0d
Lev <hotimpulse@hotmail.com>
2022-10-29 13:50:08 +0700

    docs: adding seminar1 hw

---
+++ Seminar1/hw1.java
@@ -0,0 +1,20 @@
+// 1. Написать программу вычисления n-ого треугольного числа.
+// Solution
+//---------
+// package java_class.Seminar1;
+// import java.util.Scanner;
+
+// public class hw1 {
+//     public static void main(String[] args){
+//         try (Scanner n_num = new Scanner(System.in)) {
+//             System.out.println("Enter any number to get its triangular counterpart: ");
+//             int num = n_num.nextInt();
+//             int triangular_num = 0;
+//             for (int i = 0; i <= num; i++) {
+//                 triangular_num = triangular_num + i;
+//             }
+//             System.out.println("Triangular number for " +num+ " is " +triangular_num);
+//         }
+//     }
+// }
+//---------
